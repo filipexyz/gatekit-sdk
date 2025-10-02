c8917b7a1a38282dea0c04924c54f5328778a1fa
filipexyz <luis@filipe.xyz>
2025-10-02 22:46:51 +0000

    feat: update SDK to v1.2.1

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ export * from './errors';
 
 // Version info
 export const SDK_VERSION = '1.2.1';
-export const GENERATED_AT = '2025-10-02T21:58:23.838Z';
+export const GENERATED_AT = '2025-10-02T22:42:11.028Z';
 export const CONTRACTS_COUNT = 51;
