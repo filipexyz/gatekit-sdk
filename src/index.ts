59074f2cc2ecddbe488b910967e4d111b5ea1a71
filipexyz <luis@filipe.xyz>
2025-10-05 02:49:10 +0000

    feat: update SDK to v1.3.1

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export * from './types';
 export * from './errors';
 
 // Version info
-export const SDK_VERSION = '1.3.0';
-export const GENERATED_AT = '2025-10-04T15:18:18.308Z';
+export const SDK_VERSION = '1.3.1';
+export const GENERATED_AT = '2025-10-05T02:47:11.887Z';
 export const CONTRACTS_COUNT = 53;
